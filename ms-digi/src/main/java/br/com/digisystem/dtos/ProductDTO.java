c1ee93d436181a0ffd26f46a78cb910791751d3d
Erick Kussumoto Do Nascimento <erick.knascimento@digisystem.com.br>
2022-06-01 12:11:12 -0300

    ProductsEntity + ManyToMany relation

--- ms-digi/src/main/java/br/com/digisystem/dtos/ProductDTO.java
+++ ms-digi/src/main/java/br/com/digisystem/dtos/ProductDTO.java
@@ -1,10 +1,8 @@
 package br.com.digisystem.dtos;
 
-import java.util.List;
-
 import org.modelmapper.ModelMapper;
 
-import br.com.digisystem.entities.SalesEntity;
+import br.com.digisystem.entities.ProductEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,15 +10,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class SalesDTO {
+public class ProductDTO {
 
 	private int id;
-	private double totalValue;
-	private List<ProductDTO> products;
+	private double value;
+	private String name;
+	private String description;
 	
-	public SalesEntity toEntity() {
+	public ProductEntity toEntity() {
+		
 		ModelMapper mapper = new ModelMapper();
 		
-		return mapper.map(this, SalesEntity.class);
-	}	
+		return mapper.map(this, ProductEntity.class);
+	}
 }
--- ms-digi/src/main/java/br/com/digisystem/entities/ProductEntity.java
+++ ms-digi/src/main/java/br/com/digisystem/entities/ProductEntity.java
@@ -2,19 +2,18 @@ package br.com.digisystem.entities;
 
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.modelmapper.ModelMapper;
 
-import br.com.digisystem.dtos.SalesDTO;
+import br.com.digisystem.dtos.ProductDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,25 +22,27 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="sales")
-public class SalesEntity {
+@Table(name="products")
+public class ProductEntity {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	@Column(name="total_value")
-	private double totalValue;
+	private double value;
+	private String name;
+	private String description;
 	
-	@ManyToOne
-	@JoinColumn(name="user_id")
-	private UserEntity user;
+	@ManyToMany
+	@JoinTable(name="product_sales",
+				joinColumns = @JoinColumn(name="product_id"),
+				inverseJoinColumns = @JoinColumn(name="sales_id")
+	)
+	private List<SalesEntity> sales;
 	
-	@ManyToMany(mappedBy = "sales")
-	private List<ProductEntity> products;
-	
-	public SalesDTO toDTO() {
+	public ProductDTO toDTO() {
+		
 		ModelMapper mapper = new ModelMapper();
 		
-		return mapper.map(this, SalesDTO.class);
+		return mapper.map(this, ProductDTO.class);
 	}
 }
